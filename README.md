323a7296f651e6bed0d5509e45d27609505ed291
Guilherme Bezerra <guilherme.bezerra@esig.com.br>
2023-02-28 16:27:00 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Rapidex Transportadora
---
+++ src/main/java/com/rapidexTransportadoraAPI/RapidexTransportadoraApiApplication.java
@@ -0,0 +1,13 @@
+package com.rapidexTransportadoraAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidexTransportadoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidexTransportadoraApiApplication.class, args);
+	}
+
+}
